1ab2a621717ee605c4c81e5961d0ea4f00a5b829
franrodriguezm <frarodmar17@gmail.com>
2025-11-18 13:43:39 +0100

    Update dea_multicond.R

--- short_reads_PE/scripts_PE/dea_multicond.R
+++ short_reads_PE/scripts_PE/dea_multicond.R
@@ -779,6 +779,21 @@ ggsave(paste0(DEA_results_DIR,"/BarPlotUpDown_", nm,".pdf"),
 ## Read & Preprocess GTF
 message("Loading: ", repeatmasker_annotation_gtf)
 
+              ### --- 1. Función para extraer atributos del GTF --- ###
+extract_attr <- function(x, key){
+  m <- stringr::str_match(x, paste0(key, ' "([^"]+)"'))
+  return(m[,2])
+}
+
+### --- 2. Leer GTF y extraer gene_id, repeat_class, repeat_family --- ###
+gtf_df <- rtracklayer::readGFF("repeatmasker_annotation_gtf") %>%
+  mutate(
+    gene_id       = extract_attr(attributes, "gene_id"),
+    transcript_id = extract_attr(attributes, "transcript_id"),
+    repeat_class  = extract_attr(attributes, "repeat_class"),
+    repeat_family = extract_attr(attributes, "repeat_family")
+  )
+
 ### --- 3. Filtrar solo los diferenciales --- ###
 gtf_differentials <- gtf_df %>%
   filter(gene_id %in% repeat_differentials)
@@ -834,21 +849,6 @@ for (nm in res_names) {
 library(dplyr)
 library(stringr)
 library(rtracklayer)
-
-### --- 1. Función para extraer atributos del GTF --- ###
-extract_attr <- function(x, key){
-  m <- stringr::str_match(x, paste0(key, ' "([^"]+)"'))
-  return(m[,2])
-}
-
-### --- 2. Leer GTF y extraer gene_id, repeat_class, repeat_family --- ###
-gtf_df <- rtracklayer::readGFF("repeatmasker_annotation_gtf") %>%
-  mutate(
-    gene_id       = extract_attr(attributes, "gene_id"),
-    transcript_id = extract_attr(attributes, "transcript_id"),
-    repeat_class  = extract_attr(attributes, "repeat_class"),
-    repeat_family = extract_attr(attributes, "repeat_family")
-  )
 
 
 
@@ -995,4 +995,5 @@ ggplot(df_means, aes(x = condition, y = mean_log, fill = condition)) +
 
 ggsave(paste0(DEA_results_DIR,"/repetitive_counts_violin_box.png"), width = 8, height = 6, dpi = 300)
 ggsave(paste0(DEA_results_DIR,"/repetitive_counts_violin_box.pdf"), width = 8, height = 6, dpi = 300)
+
 
