a42c1de8085d3139127cd7ce54d2b544760d6908
franrodriguezm <frarodmar17@gmail.com>
2025-11-20 11:47:02 +0100

    Update merge_quant.R

--- short_reads_PE/scripts_PE/merge_quant.R
+++ short_reads_PE/scripts_PE/merge_quant.R
@@ -13,28 +13,26 @@ print(threads)
 print(DEA_results)
 
 
-# Paquetes mínimos
+# Required packages
 library(readr)
 library(dplyr)
 library(tidyr)
 library(purrr)
 library(DESeq2)
 
-# 1) Localiza todos los quant.txt dentro de */Quantification/
+# Locate all the quant.txt inside Quantification folder 
 files <- list.files(SAMPLES_DIR, pattern = "_?quant\\.txt$", recursive = TRUE, full.names = TRUE)
 print(files)
 
-global_matrix <- matrix()  # 3 filas, 0 columnas
-
+global_matrix <- matrix()  
 
 #Load the metadata file
 file <- paste0(GLOBAL_DIR, "/", sample_list)
 print(file)
-#file.exists(file)
 
 meta <- read.table(file, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
-#Primero compruebo que los rownames son idénticos en orden en todos los txt
+#Check that rownames are ordered in the same way in all the txt files
 txt_files <- list()
 
 for (i in 1:length(files)){
@@ -63,9 +61,9 @@ todos_iguales <- all(sapply(txt_files, function(tab) {
 }))
 
 if (todos_iguales) {
-  message("✅ Todas las tablas tienen el mismo orden de rownames.")
+  message("✅ All the tables have the same rownames order.")
 } else {
-  message("⚠️ Las tablas NO tienen el mismo orden de rownames.")
+  message("⚠️ Tables do NOT have the same rownames order..")
   
 }
 
@@ -80,14 +78,14 @@ if (todos_iguales) {
   #Delete the original column of global_matrix
   global_matrix <- global_matrix[,-1]
   
-  # 5) Genera la matriz de conteo
+  #Generate count matrix
   countData <- as.data.frame(global_matrix, check.names = FALSE)
   
-  #(opcional) guarda la matriz y el objeto
+  #Save the matrix
   write.table(countData, "count_data.txt",row.names = T)
 
 }else{
-  stop("❌ La condición no se cumple. Deteniendo la ejecución.")
+  stop("❌ The condition is not met. Stopping the execution.")
 }
 
 
