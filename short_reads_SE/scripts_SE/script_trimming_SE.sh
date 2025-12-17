080220ce5461a3d3dd575f89f27c63c56dfdd0aa
franrodriguezm <frarodmar17@gmail.com>
2025-12-17 15:26:14 +0100

    Enhance script with additional trimming options
    Updated script to include new trimming parameters for quality and minimum length.

--- short_reads_SE/scripts_SE/script_trimming_SE.sh
+++ short_reads_SE/scripts_SE/script_trimming_SE.sh
@@ -4,21 +4,34 @@ sample_id="$1"
 IN="$2"
 TRIM_DIR="$3"
 adapter="$4"
-threads="$5"
+trimming_type="$5"
+threads="$6"
+trimming_quality="$7"
+min_length="$8"
 
-echo $sample_id
-echo $IN
-echo $TRIM_DIR
-echo $adapter
-echo $threads
+# Setting up colors for messages
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SCRIPT_DIR}/logging.sh"
+
+####
+
+msg_info $sample_id
+msg_info $IN
+msg_info $TRIM_DIR
+msg_info $adapter
+msg_info $trimming_type
+msg_info $threads
+msg_info $trimming_quality
+msg_info $min_length
 
 #Corro cutadapt
     # It is necessary to activate the conda environment where cutadapt software is installed
 if [ -f "trim/${sample_id}_trimmed.fastq ]; then
     echo "✅ Archivos ya existen en trim/, se omite la creación."
 else
-    cutadapt -j "$threads" --pair-filter any -q 30,30 \
-        -a "$adapter" --trim-n -m 16 \
+    cutadapt -j "$threads" --pair-filter any -q "$trimming_quality","$trimming_quality" \
+        -a "$adapter" --trim-n -m "$min_length" \
         -o "${TRIM_DIR}/${sample_id}_trimmed.fastq" \
         "$IN"  > cutadapt.log 2>&1
        
