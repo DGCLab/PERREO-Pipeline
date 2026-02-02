f4977fd9ef18238ddbfaedf07348620bd7ad23af
Francisco Rodríguez Martín <frarodmar17@gmail.com>
2026-02-02 09:01:20 +0100

    Change repeat_gtf to lnc_gtf in quant.R

--- lncRNA/scripts_perreo_lncrna/quant.R
+++ lncRNA/scripts_perreo_lncrna/quant.R
@@ -2,7 +2,7 @@ args <- commandArgs(trailingOnly=TRUE)
 
 MAP_DIR <- args[[1]]
 sample_id <- args[[2]]
-repeat_gtf <- args[[3]]
+lnc_gtf <- args[[3]]
 threads <- as.integer(args[[4]])
 strandness <- args[[5]]
 sample_dir <- args[[6]]
@@ -63,7 +63,7 @@ library(openxlsx)
 })
 
 if (!file.exists(paste0(sample_dir,"_quant.txt"))){
-     quant <- featureCounts(files = print(paste0(MAP_DIR,"/",sample_id,"_marked_duplicates_STAR.bam")), annot.ext = repeat_gtf,isGTFAnnotationFile = T, isPairedEnd = TRUE, GTF.attrType = "gene_id",countMultiMappingReads = TRUE,primaryOnly = FALSE, fraction=TRUE,strandSpecific = strandness_fc, nthreads=threads)
+     quant <- featureCounts(files = print(paste0(MAP_DIR,"/",sample_id,"_marked_duplicates_STAR.bam")), annot.ext = lnc_gtf,isGTFAnnotationFile = T, isPairedEnd = TRUE, GTF.attrType = "gene_id",countMultiMappingReads = FALSE,primaryOnly = FALSE, fraction=FALSE,strandSpecific = strandness_fc, nthreads=threads)
      msg_ok(paste0("[FEATURECOUNTS] ", sample_id,"quantification completed"))
      write.table(quant$counts,print(paste0(quant_dir,"/",sample_id,"_quant.txt")),sep="\t")
      write.table(quant$stat, print(paste0(quant_dir,"/",sample_id,"_quant_stats.txt")),sep="\t")
