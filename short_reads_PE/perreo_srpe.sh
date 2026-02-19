1ce68d01ea9ca192f219459659c5c9fd09ef3dec
Francisco Rodríguez Martín <frarodmar17@gmail.com>
2026-02-19 13:04:13 +0100

    Fix formatting and improve message output in script

--- short_reads_PE/perreo_srpe.sh
+++ short_reads_PE/perreo_srpe.sh
@@ -83,13 +83,16 @@ fi
 
 # Function for running the analysis with all the samples
 
-msg_info $' ____   _____  ____   ____   _____   ___  
+msg_info $' 
+____   _____  ____   ____   _____   ___  
 |  _ \\ | ____||  _ \\ |  _ \\ | ____| / _ \\ 
 | |_) ||  _|  | |_) || |_) ||  _|  | | | |
 |  __/ | |___ |  _ < |  _ < | |___ | |_| |
-|_|    |_____||_| \\_\\|_| \\_\\|_____| \\___/ '
+|_|    |_____||_| \\_\\|_| \\_\\|_____| \\___/ 
+'
 
-msg_info "Starting PERREO pipeline"
+msg_info "Starting PERREO pipeline
+"
 
 run_pipeline_sample() {
   msg_info " ----- Pipeline paramters summary -----"
