8df97351c4ee8b2d625c4220e9c423d6a61b5a22
Francisco Rodríguez Martín <frarodmar17@gmail.com>
2026-02-25 13:06:08 +0100

    Update script_markduplicates.sh

--- short_reads_PE/scripts_PE/script_markduplicates.sh
+++ short_reads_PE/scripts_PE/script_markduplicates.sh
@@ -27,7 +27,7 @@ source "${SCRIPT_DIR}/logging.sh"
     
       # Marking duplicates 
       msg_info "[MARKDUPLICATES] Marking duplicates for $sample_id..."
-       picard -Xmx"${ram}" MarkDuplicates \
+      picard "-Xms${ram}g -Xmx${ram}g" MarkDuplicates \
          I="$MAP_DIR/${sample_id}_with_readgroup_STAR.bam" \
          O="$MAP_DIR/${sample_id}_marked_duplicates_STAR.bam" \
          M="$MAP_DIR/${sample_id}_marked_dup_metrics_STAR.txt" \
