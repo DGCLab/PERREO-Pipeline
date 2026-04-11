b535f10471091b64f1eb2820f5049666478311fe
Francisco Rodríguez Martín <frarodmar17@gmail.com>
2026-04-11 11:07:57 +0200

    Delete Paper additional scripts/TEtranscripts_GSE147352.sh

--- Paper additional scripts/TEtranscripts_GSE147352.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/time -v TEtranscripts -t $(xargs -a GBM.txt) -c $(xargs -a HC.txt) \
-  --GTF genome_reference_nanopore_annotation_2.gtf --TE T2T_CHM13_v2_rmsk_TE.gtf --stranded reverse --mode multi \
-  --project GLIO_TEtranscripts --outdir Results --padj 0.01 --foldchange 1 --sortByPos > tetranscripts_log 2>&1
