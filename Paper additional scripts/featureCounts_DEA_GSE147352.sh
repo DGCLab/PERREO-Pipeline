5eb12e9ca5b1208202c7f196bad1931d3be50874
Francisco Rodríguez Martín <frarodmar17@gmail.com>
2026-04-11 11:08:04 +0200

    Delete Paper additional scripts/featureCounts_DEA_GSE147352.sh

--- Paper additional scripts/featureCounts_DEA_GSE147352.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/time -v ./script_featureCounts_DEA.sh -sample_list samplesheet.txt -reference_genome nanopore_genome.fa -genome_gtf genome_reference_nanopore_annotation_2.gtf \
-  -repeat_gtf repeatmasker_nanopore_annotation_3.gtf -threads 14 -trimming simple -adapt_r1 AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -adapt_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
-  -project_name "repeat_rna_glioblastoma_project" -remove_duplicates false -batch no -method DESeq2 -FDR 0.01 -positive_class "GBM" > PERREO_performance.log 2>&1
